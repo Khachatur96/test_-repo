538914ce6516cd135cd8b86f226d320b3b48b114
kgachatur96 <www.xachatur@mail.ru>
2023-01-24 20:32:25 +0400

    my firts commit

---
+++ python.py
@@ -0,0 +1,18 @@
+# file = open("text.txt")
+# print(file.readlines())
+# print(file.closed)
+# file.close()
+
+
+# with open('text.txt', 'w+') as f:
+# 	f.write('hello world !')
+# 	print(f.tell())
+# 	f.seek(0)
+# 	print(f.read())
+
+
+# import json
+# with open("text.json") as file:
+# 	x = json.load(file)
+# 	y = json.dumps(x)
+# 	print(type(y))
